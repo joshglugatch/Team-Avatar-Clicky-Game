6c1a4edef4ddd0feae824c640b86914e6e0fce86
Josh Glugatch <joshglugatch@gmail.com>
2020-11-09 19:23:36 -0800

    Create main body

--- team-avatar/src/components/Section.js
+++ team-avatar/src/components/Section.js
@@ -1,42 +1,45 @@
 import React from "react";
 import "../styles/Section.css";
-
+import Aang from "./images/aangImg200.jpg"
+import Appa from "./images/appaImg200.jpg"
+import Azula from "./images/azulaImg200.jpg"
+import Bumi from "./images/bumiImg200.jpg"
+import Iroh from "./images/irohImg200.jpg"
+import Jet from "./images/jetImg200.jpg"
+import Katara from "./images/kataraImg200.jpg"
+import Momo from "./images/momoImg200.jpg"
+import Sokka from "./images/sokkaImg200.jpg"
+import Suki from "./images/sukiImg200.jpg"
+import Toph from "./images/tophImg200.jpg"
+import Zuko from "./images/zukoImg200.jpg"
+ 
 // By importing the Section.css file, it is added to the DOM whenever this component loads
 function Section() {
   return (
-    <section className="section">
-      <h2>Lorem Ipsum Dolor Sit Amet</h2>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat.
-      </p>
+    <section className="section container">
+      
+      
+        
+          <div className="row">
+            <div className="image m-3 col" ><img className="clickMe" src={Aang} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Appa} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Azula} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Bumi} alt=""/></div>
+          </div>
+          <div className="row">
+            <div className="image m-3 col" ><img className="clickMe" src={Iroh} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Jet} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Katara} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Momo} alt=""/></div>
+          </div>
+          <div className="row">
+            <div className="image m-3 col" ><img className="clickMe" src={Sokka} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Suki} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Toph} alt=""/></div>
+            <div className="image m-3 col" ><img className="clickMe" src={Zuko} alt=""/></div>
+          </div>
+          
+      
     </section>
   );
 }
