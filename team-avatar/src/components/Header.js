34b928d3ebb19050bc824a05cee8a250e2d1e039
Josh Glugatch <joshglugatch@gmail.com>
2020-11-09 19:23:12 -0800

    create header/stylings

--- team-avatar/src/components/Header.js
+++ team-avatar/src/components/Header.js
@@ -6,7 +6,9 @@ import "../styles/Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1>Welcome</h1>
+      <h1>Welcome Last Airbender Fans!</h1>
+      <br/>
+      <h4 className="text-center text-white">Can you click all the images without repeating one?</h4>
     </header>
   );
 }
