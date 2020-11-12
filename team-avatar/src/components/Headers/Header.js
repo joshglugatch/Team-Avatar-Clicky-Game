97d9967091b1208739de2011e0d2bb5ec3ed26ab
Josh Glugatch <joshglugatch@gmail.com>
2020-11-11 16:15:31 -0800

    adjust header style, create message

--- team-avatar/src/components/Headers/Header.js
+++ team-avatar/src/components/Headers/Header.js
@@ -3,12 +3,14 @@ import "./Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
-function Header() {
+function Header(props) {
   return (
     <header className="header">
       <h1>Welcome Last Airbender Fans!</h1>
       <br/>
       <h4 className="text-center text-white">Can you click all the images without repeating one?</h4>
+      <br/>
+      <h3 className="text-center text-white">{props.message}</h3>
     </header>
   );
 }
